48c6e7e615eecf9ab011c3e7ebd66f075c8abb65
Vivvianne Kimani <ndiahav@gmail.com>
2019-08-16 10:16:27 +0300

    put the function for signals in apps.py, added an updateform in forms.py, added a save fynction for image in models.py,added an if condition in the profile function in views.py,added a form in profile.html,added paths for new post,postlistview and postdetail,added post list, post detail and post create classes in views.py,added post detail, post confirm and post form templates

--- yelp/urls.py
+++ yelp/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
+# PostUpdateView, PostDeleteView,UserPostListView
 
 urlpatterns = [
     path('', views.home, name='yelp-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.post_save, name='post-create'),
     path('about/', views.about, name='yelp-about'),
 ]
--- yelp/views.py
+++ yelp/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from django.views.generic import CreateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 
@@ -11,6 +12,37 @@ def home(request):
     }
     
     return render(request, 'yelp/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'neighbour/home.html'  
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 4
+    
+class PostDetailView(DetailView):
+    model = Post
+    
+    
+    
+def post_save(request):
+    if request.method =='POST':
+        form = PostCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            
+            post.save()
+            return redirect('neighbour-home')
+    else:
+        form = PostCreateForm()
+        
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.neighbourhood_id = self.request.neighbourhood
+        return super().form_valid(form)
+    return render(request,'neighbour/post_form.html',{'form':form})
 
 
 def about(request):
