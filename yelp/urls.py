097a1d52951e1306594ae6ec2da2914a557ced20
Vivvianne Kimani <ndiahav@gmail.com>
2019-08-16 10:59:48 +0300

    added, update and delete functions

--- yelp/urls.py
+++ yelp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView
-# PostUpdateView, PostDeleteView,UserPostListView
+from .views import PostListView, PostDetailView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', views.home, name='yelp-home'),
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', views.post_save, name='post-create'),
     path('about/', views.about, name='yelp-about'),
 ]
--- yelp/views.py
+++ yelp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from .forms import PostCreateForm
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from django.views.generic import CreateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
@@ -22,11 +25,12 @@ class PostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 4
     
+    
 class PostDetailView(DetailView):
     model = Post
     
     
-    
+@login_required  
 def post_save(request):
     if request.method =='POST':
     # u_form = UserUpdateForm(request.POST, instance=request.user)
@@ -40,6 +44,42 @@ def post_save(request):
     else:
         a_form = PostCreateForm()
     return render(request,'yelp/post_form.html',{'form':a_form})
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+    
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'neighbour/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+    
 
 
 def about(request):
