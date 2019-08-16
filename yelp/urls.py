25cffa064db5b4b1a39b41e7213df3db5719c3d1
Vivvianne Kimani <ndiahav@gmail.com>
2019-08-16 11:25:46 +0300

    added some paths,tried to add apgination

--- yelp/urls.py
+++ yelp/urls.py
@@ -3,11 +3,11 @@ from . import views
 from .views import PostListView, PostDetailView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', views.home, name='yelp-home'),
-    path('', PostListView.as_view(), name='blog-home'),
+    path('home/', views.home, name='yelp-home'),
+    path('home/', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new/', views.post_save, name='post-create'),
-    path('about/', views.about, name='yelp-about'),
+    path('', views.about, name='yelp-about'),
 ]
